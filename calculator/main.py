fc8c1ad94cfbccdb8ff71a06a7a740e76705a6da
shilpa <shilpaparameshwara1997@gmail.com>
2021-11-02 11:19:37 -0400

    updated calculator prgram

--- calculator/main.py
+++ calculator/main.py
@@ -1,25 +1,27 @@
-""" This is the increment function"""
+""" This is the  increment function"""
+number_types = (int, float, complex)
+""" This is the  data type function"""
 class Calculator:
     """ This is the Calculator class"""
 
-    result = 0
-    def get_result(self):
-        """ Get Result of Calculation"""
-        return self.result
-
-    def add_number(self, value_a):
-        """ adds number to result"""
-        self.result = self.result + value_a
-        return self.result
-    def subtract_number(self, value_a):
+    @staticmethod
+    def validate_args(value_x, value_y):
+        """ This is to validate the arguments"""
+        if not isinstance(value_x, number_types) and not isinstance(value_y, number_types):
+            raise ValueError
+    def add(self, value_x, value_y):
+        """ addition of number from result"""
+        self.validate_args(value_x, value_y)
+        return value_x + value_y
+    def sub(self, value_x, value_y):
         """ subtract number from result"""
-        self.result = self.result - value_a
-        return self.result
-    def multiply_numbers(self, value_a, value_b):
+        self.validate_args(value_x, value_y)
+        return value_x - value_y
+    def multiply(self, value_x, value_y):
         """ multiply two Numbers and store the result"""
-        self.result = value_a * value_b
-        return self.result
-    def div_numbers(self, value_a, value_b):
+        self.validate_args(value_x, value_y)
+        return value_x * value_y
+    def div(self, value_x, value_y):
         """ divide two Numbers and store the result"""
-        self.result = value_a / value_b
-        return self.result
+        self.validate_args(value_x, value_y)
+        return value_x / value_y
--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,38 +1,53 @@
-"""Testing the Calculator"""
+"""importing unitest for testcases"""
+import unittest
 from calculator.main import Calculator
+class CalculatorTests(unittest.TestCase):
+    """testing unittest of the calculator"""
+    def setUp(self):
+        """setting the self function of the calculator"""
+        self.calculator = Calculator()
 
-def test_calculator_result():
-    """testing calculator result is 0"""
-    calc = Calculator()
-    assert calc.result == 0
 
-def test_calculator_add():
-    """Testing the Add function of the calculator"""
-    #Arrange by instantiating the calc class
-    calc = Calculator()
-    #Act by calling the method to be tested
-    calc.add_number(4)
-    #Assert that the results are correct
-    assert calc.result == 4
+    def test_add_method(self):
+        """Testing the Add function of the calculator"""
+        result = self.calculator.add(4, 2)
+        self.assertEqual(6, result)
 
-def test_calculator_get_result():
-    """Testing the Get result method of the calculator"""
-    calc = Calculator()
-    assert calc.get_result() == 0
+    def test_add_method_invalid_value(self):
+        """Testing the Add invalid function of the calculator"""
+        self.assertRaises(ValueError, self.calculator.add, "four", "five")
 
-def test_calculator_subtract():
-    """Testing the subtract method of the calculator"""
-    calc = Calculator()
-    calc.subtract_number(1)
-    assert calc.get_result() == -1
-def test_calculator_multiply():
-    """ tests multiplication of two Numbers"""
-    calc = Calculator()
-    result  = calc.multiply_numbers(1,2)
-    assert result == 2
+    def test_sub_method(self):
+        """Testing the sub function of the calculator"""
+        result = self.calculator.sub(6, 4)
+        self.assertEqual(2, result)
 
-def test_calculator_div():
-    """ tests multiplication of two Numbers"""
-    calc = Calculator()
-    result = calc.div_numbers(4, 2)
-    assert result == 2
+    def test_sub_method_invalid_value(self):
+        """Testing the sub invalid function of the calculator"""
+        self.assertRaises(ValueError, self.calculator.sub, "four", "five")
+
+    def test_multiply_method(self):
+        """Testing the mul function of the calculator"""
+        result = self.calculator.multiply(5, 3)
+        self.assertEqual(15, result)
+
+    def test_multiply_method_invalid_value(self):
+        """Testing the mul invalid function of the calculator"""
+        self.assertRaises(ValueError, self.calculator.multiply, "four", "five")
+
+    def test_div_method(self):
+        """Testing the div function of the calculator"""
+        result = self.calculator.div(5, 1)
+        self.assertEqual(5, result)
+
+    def test_div_method_invalid_value(self):
+        """Testing the div invalid function of the calculator"""
+        self.assertRaises(ValueError, self.calculator.div, "five", "four")
+
+    def test_div_method_zero(self):
+        """Testing the div zero function of the calculator"""
+        self.assertRaises(ZeroDivisionError, self.calculator.div, 5, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
