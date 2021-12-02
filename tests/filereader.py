018b3ee401fd6d843ff6fadd8ade425d3baf63e0
nk398 <nk398@njit.edu>
2021-12-02 02:55:59 -0500

    updated csv files, reading and writing

--- tests/filereader.py
+++ tests/filereader.py
@@ -4,7 +4,7 @@ from filewriter import Filewriter
 #pylint: disable=import-error
 
 # pylint: disable=trailing-newlines,missing-class-docstring,missing-function-docstring,no-self-argument,too-few-public-methods
-class FileReader():
+class FileReader(): 
     def readcsv(filepath):
         file = pd.read_csv(filepath)
         return Filewriter(file)
